ded3c9ddd5451b9d71233338307d091d7b56dbbf
Kumar Aditya <32020813+kmradi@users.noreply.github.com>
2022-12-07 15:10:50 +0530

    Add files via upload

---
+++ Stop_Apps.ps1
@@ -0,0 +1,17 @@
+﻿$interval = 6;
+$counter = 10;
+
+for(; $counter -gt 0; $counter--)
+{
+    $processList = Get-Process "chrome","lghub" -ErrorAction SilentlyContinue;
+    if($processList)
+    {
+        Stop-Process $processList -Force;
+        Get-Process | Where-Object {$_.HasExited};
+    }
+
+    Write-Host $Counter;
+    if($counter -eq 1 ) 
+        { break }
+    Start-Sleep -Seconds $interval;
+}
